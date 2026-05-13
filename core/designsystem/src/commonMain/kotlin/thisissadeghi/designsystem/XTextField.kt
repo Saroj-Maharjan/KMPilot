9b6914052a8cd0a6aa7615336b61e7fb68b182ad
ThisIsSadeghi <alisadeghi.dev@gmail.com>
2026-05-14 00:58:43 +0330

    Remove unnecessary `ExperimentalMaterial3Api` annotation from `XTextField`.

--- core/designsystem/src/commonMain/kotlin/thisissadeghi/designsystem/XTextField.kt
+++ core/designsystem/src/commonMain/kotlin/thisissadeghi/designsystem/XTextField.kt
@@ -11,7 +11,6 @@ import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.BasicTextField
 import androidx.compose.foundation.text.KeyboardActions
 import androidx.compose.foundation.text.KeyboardOptions
-import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.LocalTextStyle
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextFieldDefaults
@@ -31,7 +30,6 @@ import androidx.compose.ui.text.input.VisualTransformation
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 
-@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun XTextField(
     value: String,
